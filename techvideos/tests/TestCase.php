38b6bc46ebdb42bac6dce16aceacd264d889fc1d
Alejandro Estringana Ruiz <estringana@gmail.com>
2017-03-22 20:56:52 +1100

    Add helper function for testing
    This function stop laravel from translating php errors into http errors

--- techvideos/tests/TestCase.php
+++ techvideos/tests/TestCase.php
@@ -6,5 +6,21 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+	use CreatesApplication;
+
+	protected function disableExceptionHandling()
+	{
+		$this->app->instance(ExceptionHandler::class, new class extends Handler {
+			public function __construct()
+			{
+			}
+			public function report(Exception $e)
+			{
+			}
+			public function render($request, Exception $e)
+			{
+				throw $e;
+			}
+		});
+	}
 }
