e5a525207904f80dcee14eaa9ed534184903d633
Alejandro Estringana Ruiz <alejandro.ruiz@theiconic.com.au>
2017-12-05 08:34:35 +1100

    Add fillable fields to user

--- app/User.php
+++ app/User.php
@@ -10,6 +10,24 @@ class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
     public function votes()
     {
         return $this->hasMany(Vote::class);
